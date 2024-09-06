b409dc160520d4555d14921c4f9d806ee4483408
Thomas <thomas.kiley@ndm.ox.ac.uk>
2024-09-06 09:53:18 +0100

    Split the post processing pipeline into two functions

--- endgame_postprocessing/model_wrappers/lf/testRun.py
+++ endgame_postprocessing/model_wrappers/lf/testRun.py
@@ -21,38 +21,23 @@ from tqdm import tqdm
 import pandas as pd
 
 
-def run_postprocessing_pipeline(input_dir: str, output_dir: str):
+def run_aggregate_runs_for_each_iu(input_dir: str, output_iu_dir: str):
     """
-    Aggregates into standard format the input files found in input_dir.
+    Aggregates the stochastic runs (into mean etc)
+    into standard format the input files found in input_dir.
     input_dir must have the following substructure:
         scenario1/
             country1/
                 iu1/
                     iu.csv
         scenario2/
-
     The output directory must be empty.
     On completion the sub-structure will be:
-    output_dir/
-        ius/
-            a csv per IU with name format
-            scenario1_iu1_post_processed.csv
-        aggregated/
-            combined-lf-iu-lvl-agg.csv - all IUs in one csv
-                a aggregated by country csv
-            combined-lf-country-lvl-agg.csv - aggregate by country
-            combined-lf-africa-lvl-agg.csv - aggregated across Africa
-        ...
-    Arguments:
-        input_dir (str): The directory to search for input files.
-        output_dir (str): The directory to store the output files.
-
+    output_iu_dir/
+        a csv per IU with name format
+        scenario1_iu1_post_processed.csv
     """
-    output_iu_dir = f"{output_dir}/ius/"
     os.makedirs(output_iu_dir)
-    output_aggregate_dir = f"{output_dir}/aggregated/"
-    os.makedirs(output_aggregate_dir)
-
     file_iter = post_process_file_generator(
         file_directory=input_dir, end_of_file=".csv"
     )
@@ -75,7 +60,26 @@ def run_postprocessing_pipeline(input_dir: str, output_dir: str):
                 pbar, file_info.scenario_index, file_info.total_scenarios
             )
 
-    combined_ius = aggregate_post_processed_files(output_iu_dir)
+
+def generate_aggregates_for_all_ius(processed_iu_dir: str, output_aggregate_dir: str):
+    """
+    Generates the aggregate files - one for all IUs, one for all countries, one for Africa.
+    Expected to be run after run_aggregate_runs_for_each_iu
+
+    processed_iu_dir must have the following substructure:
+    processed_iu_dir/
+        a csv per IU with name format
+        scenario1_iu1_post_processed.csv
+        The output directory must be empty.
+    On completion the sub-structure will be:
+    output_aggregate_dir/
+        combined-lf-iu-lvl-agg.csv - all IUs in one csv
+            a aggregated by country csv
+        combined-lf-country-lvl-agg.csv - aggregate by country
+        combined-lf-africa-lvl-agg.csv - aggregated across Africa
+    """
+    os.makedirs(output_aggregate_dir)
+    combined_ius = aggregate_post_processed_files(processed_iu_dir)
     aggregated_df = iu_lvl_aggregate(combined_ius)
     aggregated_df.to_csv(f"{output_aggregate_dir}/combined-lf-iu-lvl-agg.csv")
     country_lvl_data = country_lvl_aggregate(
@@ -99,6 +103,38 @@ def run_postprocessing_pipeline(input_dir: str, output_dir: str):
         measures_to_summarize=["sampled mf prevalence (all pop)"],
         columns_to_group_by=constants.AFRICA_LVL_GROUP_COLUMNS,
     ).to_csv(f"{output_aggregate_dir}/combined-lf-africa-lvl-agg.csv")
+
+
+def run_postprocessing_pipeline(input_dir: str, output_dir: str):
+    """
+    Aggregates into standard format the input files found in input_dir.
+    input_dir must have the following substructure:
+        scenario1/
+            country1/
+                iu1/
+                    iu.csv
+        scenario2/
+
+    The output directory must be empty.
+    On completion the sub-structure will be:
+    output_dir/
+        ius/
+            a csv per IU with name format
+            scenario1_iu1_post_processed.csv
+        aggregated/
+            combined-lf-iu-lvl-agg.csv - all IUs in one csv
+                a aggregated by country csv
+            combined-lf-country-lvl-agg.csv - aggregate by country
+            combined-lf-africa-lvl-agg.csv - aggregated across Africa
+    Arguments:
+        input_dir (str): The directory to search for input files.
+        output_dir (str): The directory to store the output files.
+
+    """
+    output_iu_dir = f"{output_dir}/ius/"
+    run_aggregate_runs_for_each_iu(input_dir, output_iu_dir)
+    output_aggregate_dir = f"{output_dir}/aggregated/"
+    generate_aggregates_for_all_ius(output_iu_dir, output_aggregate_dir)
 
 
 if __name__ == "__main__":
