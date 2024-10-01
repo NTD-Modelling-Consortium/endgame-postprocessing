5b00ff43d075cd6f23d6c21ec115196fa5ce8457
Thomas <thomas.kiley@ndm.ox.ac.uk>
2024-10-01 14:22:19 +0100

    Canonicalise the oncho data before anything else

--- endgame_postprocessing/model_wrappers/oncho/testRun.py
+++ endgame_postprocessing/model_wrappers/oncho/testRun.py
@@ -1,3 +1,4 @@
+from endgame_postprocessing.post_processing import canonicalise
 from endgame_postprocessing.post_processing.aggregation import (
     add_scenario_and_country_to_raw_data,
     aggregate_post_processed_files,
@@ -20,7 +21,26 @@ from tqdm import tqdm
 import pandas as pd
 
 
-oncho_dir = "input-data/oncho/"
+def canonicalise_raw_oncho_results(input_dir, output_dir):
+    file_iter = post_process_file_generator(
+        file_directory=input_dir, end_of_file="-raw_all_age_data.csv"
+    )
+
+    all_files = list(file_iter)
+
+    for file_info in tqdm(all_files, desc="Canoncialise oncho results"):
+        raw_iu = pd.read_csv(file_info.file_path)
+        raw_iu.drop(columns=["age_start", "age_end"])
+        canonical_result = canonicalise.canonicalise_raw(
+            raw_iu, file_info, "prevalence"
+        )
+        canonicalise.write_canonical(output_dir, file_info, canonical_result)
+
+
+oncho_dir = "local_data/smoncho"
+
+canonicalise_raw_oncho_results(oncho_dir, "local_data/output")
+
 file_iter = post_process_file_generator(
     file_directory=oncho_dir, end_of_file="-raw_all_age_data.csv"
 )
--- endgame_postprocessing/post_processing/canonicalise.py
+++ endgame_postprocessing/post_processing/canonicalise.py
@@ -1,3 +1,5 @@
+import os
+from pathlib import Path
 import pandas as pd
 
 from endgame_postprocessing.post_processing.dataclasses import CustomFileInfo
@@ -11,7 +13,7 @@ def canonicalise_raw(
     raw.insert(1, canoncical_columns.COUNTRY_CODE, file_info.country)
     raw.insert(2, canoncical_columns.IU_NAME, file_info.iu)
     filtered_data = raw.query(f"measure == '{processed_prevalence_name}'")
-    filtered_data["measure"] = canoncical_columns.PROCESSED_PREVALENCE
+    filtered_data.loc[:, "measure"] = canoncical_columns.PROCESSED_PREVALENCE
 
     if canoncical_columns.YEAR_ID not in raw.columns:
         raise Exception(f"Could not find {canoncical_columns.YEAR_ID} column")
@@ -20,3 +22,15 @@ def canonicalise_raw(
         raise Exception(f"Could not find {canoncical_columns.MEASURE} column")
 
     return filtered_data.reset_index(drop=True)
+
+
+def write_canonical(
+    root_dir, file_info: CustomFileInfo, canonical_result: pd.DataFrame
+):
+    scenario = file_info.scenario
+    country = file_info.country
+    iu = file_info.iu
+    file_name = f"canonical_{iu}.csv"
+    path = Path(f"{root_dir}/canonical_results/{scenario}/{country}/")
+    path.mkdir(parents=True, exist_ok=True)
+    canonical_result.to_csv(f"{path}/{file_name}")
