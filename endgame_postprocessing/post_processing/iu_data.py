d40b0e41c289b1f96934c4afdf3bce5e039c2f07
Thomas <thomas.kiley@ndm.ox.ac.uk>
2024-10-10 16:50:08 +0100

    Swap to using dataframe for underlying data

--- endgame_postprocessing/post_processing/iu_data.py
+++ endgame_postprocessing/post_processing/iu_data.py
@@ -1,5 +1,7 @@
 import re
 
+import pandas as pd
+
 
 def _is_valid_iu_code(iu_code):
     return re.match("[A-Z]{3}\d{5}", iu_code)
@@ -7,12 +9,20 @@ def _is_valid_iu_code(iu_code):
 
 class IUData:
 
-    def __init__(self, input_data):
+    def __init__(self, input_data: pd.DataFrame):
+        if input_data["IU_CODE"].nunique() != len(input_data):
+            raise InvalidIUDataFile("Duplicate IUs found")
         self.input_data = input_data
 
     def get_priority_population_for_IU(self, iu_code):
         if not _is_valid_iu_code(iu_code):
             raise Exception(f"Invalid IU code: {iu_code}")
-        if iu_code in self.input_data:
-            return self.input_data[iu_code]
-        return 10000
+        iu = self.input_data.loc[self.input_data.IU_CODE == iu_code]
+        if len(iu) == 0:
+            raise Exception(f"IU {iu_code} not found in IU metadata file")
+        assert len(iu) == 1
+        return iu["population"].iat[0]
+
+
+class InvalidIUDataFile(Exception):
+    pass
--- tests/post_processing/test_iu_data.py
+++ tests/post_processing/test_iu_data.py
@@ -1,15 +1,29 @@
+import pandas as pd
 import pytest
-from endgame_postprocessing.post_processing.iu_data import IUData
+from endgame_postprocessing.post_processing.iu_data import IUData, InvalidIUDataFile
 
 
-def test_iu_data_get_priority_population_iu_missing():
-    assert IUData({}).get_priority_population_for_IU("AAA00001") == 10000
+def test_iu_data_get_priority_population_iu_missing_raises_exception():
+    with pytest.raises(Exception):
+        IUData(pd.DataFrame({"IU_CODE": []})).get_priority_population_for_IU("AAA00001")
 
 
 def test_iu_data_get_priority_population_invalid_iu_raises_exception():
     with pytest.raises(Exception):
-        IUData({}).get_priority_population_for_IU("AAA0001")
+        IUData(pd.DataFrame({"IU_CODE": []})).get_priority_population_for_IU("AAA0001")
 
 
 def test_iu_data_get_priority_population_iu_in():
-    assert IUData({"AAA00001": 10}).get_priority_population_for_IU("AAA00001") == 10
+    assert (
+        IUData(
+            pd.DataFrame({"IU_CODE": ["AAA00001"], "population": [10]})
+        ).get_priority_population_for_IU("AAA00001")
+        == 10
+    )
+
+
+def test_duplicate_iu_raises_exception():
+    with pytest.raises(InvalidIUDataFile):
+        IUData(
+            pd.DataFrame({"IU_CODE": ["AAA00001", "AAA00001"], "population": [10, 20]})
+        )
