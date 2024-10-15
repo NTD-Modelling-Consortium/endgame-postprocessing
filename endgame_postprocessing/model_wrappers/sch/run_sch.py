be261facc54f4da31f669c5104bb21666a23e29e
Thomas <thomas.kiley@ndm.ox.ac.uk>
2024-10-15 17:53:04 +0100

    Put together many worms

--- endgame_postprocessing/model_wrappers/sch/run_sch.py
+++ endgame_postprocessing/model_wrappers/sch/run_sch.py
@@ -1,6 +1,15 @@
 from functools import reduce
 from operator import mul
-
+import os
+from tqdm import tqdm
+from endgame_postprocessing.post_processing import (
+    canonicalise,
+    output_directory_structure,
+)
+from endgame_postprocessing.post_processing.file_util import (
+    post_process_file_generator,
+)
+import pandas as pd
 
 def probability_any_worm(probability_for_each_worm):
     """
@@ -11,3 +20,55 @@ def probability_any_worm(probability_for_each_worm):
     )
     prob_not_any_worm = reduce(mul, prob_of_not_each_worm, 1.0)
     return 1.0 - prob_not_any_worm
+
+
+def canoncialise_single_result(file_info):
+    raw_iu = pd.read_csv(file_info.file_path)
+    raw_without_columns = raw_iu.drop(columns=["espen_loc"])
+    # TODO: canonical shouldn't need the age_start / age_end but these are assumed present later
+    return canonicalise.canonicalise_raw(
+        raw_without_columns, file_info, "true mf prevalence (all pop)"
+    )
+
+
+def combine_many_worms(first_worm, other_worms):
+    other_worm_draws = [other_worm.loc[:, "draw_0":] for other_worm in other_worms]
+    first_worm.loc[:, "draw_0":] = probability_any_worm(
+        [first_worm.loc[:, "draw_0":]] + other_worm_draws
+    )
+    return first_worm
+
+
+def canonicalise_raw_sth_results(input_dir, output_dir):
+    worms = next(os.walk("."))[1]
+
+    first_worm = worms[0]
+    other_worms = worms[1:]
+    file_iter = post_process_file_generator(
+        file_directory=f"{input_dir}/{first_worm}", end_of_file=".csv"
+    )
+
+    all_files = list(file_iter)
+
+    if len(all_files) == 0:
+        raise Exception(
+            "No data for IUs found - see above warnings and check input directory"
+        )
+
+    for file_info in tqdm(all_files, desc="Canoncialise STH results"):
+        canonical_result_first_worm = canoncialise_single_result(file_info)
+        other_worm_paths = [
+            file_info.file_path.replace(first_worm, worm) for worm in other_worms
+        ]
+
+        other_worms_canoncial = [
+            canoncialise_single_result(other_worm_path)
+            for other_worm_path in other_worm_paths
+        ]
+
+        all_worms_canonical = combine_many_worms(
+            canonical_result_first_worm, other_worms_canoncial
+        )
+        output_directory_structure.write_canonical(
+            output_dir, file_info, all_worms_canonical
+        )
--- tests/model_wrappers/sch/test_run_sch.py
+++ tests/model_wrappers/sch/test_run_sch.py
@@ -1,4 +1,9 @@
-from endgame_postprocessing.model_wrappers.sch.run_sch import probability_any_worm
+import pandas as pd
+import pandas.testing as pdt
+from endgame_postprocessing.model_wrappers.sch.run_sch import (
+    combine_many_worms,
+    probability_any_worm,
+)
 
 
 def test_probability_any_worm_zero_for_all_worms():
@@ -11,3 +16,75 @@ def test_probability_any_worm_one_for_one_worm():
 
 def test_probability_any_worm_half_for_all_worms():
     assert probability_any_worm([0.5, 0.5, 0.5]) == 1.0 - 0.125
+
+
+def test_combine_many_worms():
+    first_worm = pd.DataFrame(
+        {
+            "year": [2010],
+            "draw_0": [0.5],
+            "draw_1": [0.0],
+        }
+    )
+
+    second_worm = pd.DataFrame(
+        {
+            "year": [2010],
+            "draw_0": [0.5],
+            "draw_1": [0.0],
+        }
+    )
+
+    third_worm = pd.DataFrame(
+        {
+            "year": [2010],
+            "draw_0": [0.5],
+            "draw_1": [1.0],
+        }
+    )
+    pdt.assert_frame_equal(
+        combine_many_worms(first_worm, [second_worm, third_worm]),
+        pd.DataFrame(
+            {
+                "year": [2010],
+                "draw_0": [1 - 0.125],
+                "draw_1": [1.0],
+            }
+        ),
+    )
+
+
+def test_combine_many_worms_many_years():
+    first_worm = pd.DataFrame(
+        {
+            "year": [2010, 2011],
+            "draw_0": [0.5, 0.5],
+            "draw_1": [0.0, 0.0],
+        }
+    )
+
+    second_worm = pd.DataFrame(
+        {
+            "year": [2010, 2011],
+            "draw_0": [0.5, 0.5],
+            "draw_1": [0.0, 0.0],
+        }
+    )
+
+    third_worm = pd.DataFrame(
+        {
+            "year": [2010, 2011],
+            "draw_0": [0.5, 0.5],
+            "draw_1": [1.0, 0.0],
+        }
+    )
+    pdt.assert_frame_equal(
+        combine_many_worms(first_worm, [second_worm, third_worm]),
+        pd.DataFrame(
+            {
+                "year": [2010, 2011],
+                "draw_0": [1 - 0.125] * 2,
+                "draw_1": [1.0, 0.0],
+            }
+        ),
+    )
