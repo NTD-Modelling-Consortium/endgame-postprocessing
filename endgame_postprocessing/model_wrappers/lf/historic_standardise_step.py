46a5477596b877fa03aad36315af6263aebe38bc
Thomas <thomas.kiley@ndm.ox.ac.uk>
2024-12-02 17:46:02 +0000

    Fix errors in historic standardise

--- endgame_postprocessing/model_wrappers/lf/historic_standardise_step.py
+++ endgame_postprocessing/model_wrappers/lf/historic_standardise_step.py
@@ -4,7 +4,6 @@ import re
 import warnings
 
 import pandas as pd
-from endgame_postprocessing.model_wrappers.lf.testRun import canonicalise_raw_lf_results
 from endgame_postprocessing.post_processing import file_util, pipeline
 from endgame_postprocessing.post_processing.dataclasses import CustomFileInfo
 from endgame_postprocessing.post_processing.disease import Disease
@@ -32,15 +31,15 @@ def perform_historic_standardise_step(nonstandard_input, raw_output):
     Renaming the year to the conventional year_id
     Naming the file according to the usual convention, labelling as scenario_0
     """
-    Path(raw_input_data).mkdir(parents=True, exist_ok=True)
-    for matt_file in get_lf_matt_flat(input_dir):
+    Path(raw_output).mkdir(parents=True, exist_ok=True)
+    for matt_file in get_lf_matt_flat(nonstandard_input):
         raw_iu = pd.read_csv(matt_file.file_path)
         raw_iu.insert(2, "measure", "true mf prevalence (all pop)")
         raw_iu.insert(2, "age_start", 5)
         raw_iu.insert(2, "age_end", 100)
         raw_iu_renamed = raw_iu.rename(columns={"year": "year_id"})
         raw_iu_renamed.to_csv(
-            f"{raw_input_data}/ntdmc-{matt_file.iu}-lf-scenario_0-200.csv",
+            f"{raw_output}/ntdmc-{matt_file.iu}-lf-scenario_0-200.csv",
             index=False,
             float_format="%g",
         )
