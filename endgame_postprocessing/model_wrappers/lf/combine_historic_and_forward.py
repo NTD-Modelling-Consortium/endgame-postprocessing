54d0d8ff43f046f6b76fde0834e04e63c6e99b35
Thomas <thomas.kiley@ndm.ox.ac.uk>
2024-12-03 11:19:17 +0000

    Raise an error if the columns don't match

--- endgame_postprocessing/model_wrappers/lf/combine_historic_and_forward.py
+++ endgame_postprocessing/model_wrappers/lf/combine_historic_and_forward.py
@@ -10,6 +10,16 @@ class MissingHistoricDataException(Exception):
         super().__init__(f"Missing IU: {iu} in historic data")
 
 
+class MismatchedColumnsException(Exception):
+    def __init__(self, iu):
+        super().__init__(f"{iu} different columns in historic and forward projection")
+
+
+def _all_columns_match(df1: pd.DataFrame, df2: pd.DataFrame):
+    matching_columns = df1.columns.intersection(df2.columns)
+    return len(matching_columns) == len(df1.columns)
+
+
 def combine_historic_and_forward(
     historic_canonical_data_path, forward_canonical_data_path, output_path
 ):
@@ -31,6 +41,10 @@ def combine_historic_and_forward(
         historic_file = historic_data_file_infos[forward_file.iu]
         historic_data = pd.read_csv(historic_file.file_path)
         forward_data = pd.read_csv(forward_file.file_path)
+
+        if not _all_columns_match(historic_data, forward_data):
+            raise (MismatchedColumnsException(forward_file.iu))
+
         all_data = pd.concat([historic_data, forward_data])
         all_data["scenario"] = forward_file.scenario
         output_directory_structure.write_canonical(output_path, forward_file, all_data)
