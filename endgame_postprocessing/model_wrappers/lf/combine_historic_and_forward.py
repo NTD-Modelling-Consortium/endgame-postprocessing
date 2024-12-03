a8ecaec77bbf38853b04238200984ff34225cce1
Thomas <thomas.kiley@ndm.ox.ac.uk>
2024-12-03 10:36:16 +0000

    Remove unneeded code

--- endgame_postprocessing/model_wrappers/lf/combine_historic_and_forward.py
+++ endgame_postprocessing/model_wrappers/lf/combine_historic_and_forward.py
@@ -24,17 +24,6 @@ def combine_historic_and_forward(
         historic_file = historic_data_file_infos[forward_file.iu]
         historic_data = pd.read_csv(historic_file.file_path)
         forward_data = pd.read_csv(forward_file.file_path)
-        # scenario = forward_data[canoncical_columns.SCENARIO].iat[0]
         all_data = pd.concat([historic_data, forward_data])
-        # new_file = dataclasses.replace(forward_file, scenario=scenario)
         historic_data["scenario"] = forward_file.scenario
         output_directory_structure.write_canonical(output_path, forward_file, all_data)
-
-
-if __name__ == "__main__":
-    historic_canoncial = "local_data/lf-historic-output/canonical_results"
-    forward_canoncical = "local_data/lf-output/canonical_results"
-    combined_output = "local_data/lf-combined-output"
-    combine_historic_and_forward(
-        historic_canoncial, forward_canoncical, combined_output
-    )
