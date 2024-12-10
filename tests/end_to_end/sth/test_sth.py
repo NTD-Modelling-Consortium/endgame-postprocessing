13e882e6c9c68614cfdd04aedc0ec016ce78cd0b
Thomas <thomas.kiley@ndm.ox.ac.uk>
2024-12-10 14:46:16 +0000

    Pull out ocmmon path for end to end test

--- tests/end_to_end/sth/test_sth.py
+++ tests/end_to_end/sth/test_sth.py
@@ -17,6 +17,7 @@ def test_sth_end_to_end_no_historic_one_worm(snapshot):
     test_root = Path(__file__).parent / data_dir
     input_data = test_root / "example_input_data"
     output_path = test_root / "generated_data"
+    known_good_subpath = "known_good_output"
 
     if output_path.exists():
         shutil.rmtree(output_path)
@@ -35,14 +36,14 @@ def test_sth_end_to_end_no_historic_one_worm(snapshot):
 
     results = sorted(generate_flat_snapshot_set(output_path))
     expected_results = sorted(
-        generate_flat_snapshot_set(test_root / "known_good_output")
+        generate_flat_snapshot_set(test_root / known_good_subpath)
     )
 
     snapshot.snapshot_dir = test_root
 
     if results != expected_results:
         snapshot.assert_match_dir(
-            generate_snapshot_dictionary(output_path), "known_good_output"
+            generate_snapshot_dictionary(output_path), known_good_subpath
         )
         # If updating we want to stop here
         return
@@ -50,7 +51,7 @@ def test_sth_end_to_end_no_historic_one_worm(snapshot):
     for actual_file_path, expected_file_path in zip(results, expected_results):
         assert actual_file_path == expected_file_path
         full_actual_path = output_path / actual_file_path
-        full_expected_file_path = test_root / "known_good_output" / expected_file_path
+        full_expected_file_path = test_root / known_good_subpath / expected_file_path
 
         actual_csv = pd.read_csv(full_actual_path)
         expected_csv = pd.read_csv(full_expected_file_path)
@@ -64,7 +65,7 @@ def test_sth_end_to_end_no_historic_one_worm(snapshot):
             print(f"Mismatch in file {actual_file_path}:", file=sys.stderr)
             print(pandas_error, file=sys.stderr)
             snapshot.assert_match_dir(
-                generate_snapshot_dictionary(output_path), "known_good_output"
+                generate_snapshot_dictionary(output_path), known_good_subpath
             )
 
 
@@ -73,6 +74,7 @@ def test_sth_end_to_end_no_historic_many_worms(snapshot):
     test_root = Path(__file__).parent / data_dir
     input_data = test_root / "example_input_data"
     output_path = test_root / "generated_data_combined_worms"
+    known_good_subpath = "known_good_output_combined_worms"
 
     if output_path.exists():
         shutil.rmtree(output_path)
@@ -94,7 +96,7 @@ def test_sth_end_to_end_no_historic_many_worms(snapshot):
 
     results = sorted(generate_flat_snapshot_set(output_path))
     expected_results = sorted(
-        generate_flat_snapshot_set(test_root / "known_good_output_combined_worms")
+        generate_flat_snapshot_set(test_root / known_good_subpath)
     )
 
     snapshot.snapshot_dir = test_root
@@ -102,7 +104,7 @@ def test_sth_end_to_end_no_historic_many_worms(snapshot):
     if results != expected_results:
         snapshot.assert_match_dir(
             generate_snapshot_dictionary(output_path),
-            "known_good_output_combined_worms",
+            known_good_subpath,
         )
         # If updating we want to stop here
         return
@@ -110,8 +112,9 @@ def test_sth_end_to_end_no_historic_many_worms(snapshot):
     for actual_file_path, expected_file_path in zip(results, expected_results):
         assert actual_file_path == expected_file_path
         full_actual_path = output_path / actual_file_path
+
         full_expected_file_path = (
-            test_root / "known_good_output_combined_worms" / expected_file_path
+            test_root / known_good_subpath / expected_file_path
         )
 
         actual_csv = pd.read_csv(full_actual_path)
@@ -127,5 +130,5 @@ def test_sth_end_to_end_no_historic_many_worms(snapshot):
             print(pandas_error, file=sys.stderr)
             snapshot.assert_match_dir(
                 generate_snapshot_dictionary(output_path),
-                "known_good_output_combined_worms",
+                known_good_subpath,
             )
