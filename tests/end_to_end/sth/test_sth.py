59ac3226da5b4949b9b9952e1e4e2e1f17c4bfb4
Thomas <thomas.kiley@ndm.ox.ac.uk>
2024-12-10 12:21:55 +0000

    Add test for checking STH results not changing

---
+++ tests/end_to_end/sth/test_sth.py
@@ -0,0 +1,131 @@
+import shutil
+import sys
+
+
+from pathlib import Path
+import pandas as pd
+import pandas.testing as pdt
+
+from endgame_postprocessing.model_wrappers.sch import run_sch
+from tests.end_to_end.generate_snapshot_dictionary import (
+    generate_flat_snapshot_set,
+    generate_snapshot_dictionary,
+)
+
+def test_sth_end_to_end_no_historic_one_worm(snapshot):
+    data_dir = "data_no_historic"
+    test_root = Path(__file__).parent / data_dir
+    input_data = test_root / "example_input_data"
+    output_path = test_root / "generated_data"
+
+    if output_path.exists():
+        shutil.rmtree(output_path)
+
+    run_sch.run_sth_postprocessing_pipeline(
+        input_data,
+        output_dir=output_path,
+        worm_directories=["ascaris"],
+        num_jobs=1,
+        run_country_level_summaries=True,
+    )
+
+    # # Composite data is not part of the interface so don't check
+    composite_path = output_path / "composite"
+    shutil.rmtree(composite_path)
+
+    results = sorted(generate_flat_snapshot_set(output_path))
+    expected_results = sorted(
+        generate_flat_snapshot_set(test_root / "known_good_output")
+    )
+
+    snapshot.snapshot_dir = test_root
+
+    if results != expected_results:
+        snapshot.assert_match_dir(
+            generate_snapshot_dictionary(output_path), "known_good_output"
+        )
+        # If updating we want to stop here
+        return
+
+    for actual_file_path, expected_file_path in zip(results, expected_results):
+        assert actual_file_path == expected_file_path
+        full_actual_path = output_path / actual_file_path
+        full_expected_file_path = test_root / "known_good_output" / expected_file_path
+
+        actual_csv = pd.read_csv(full_actual_path)
+        expected_csv = pd.read_csv(full_expected_file_path)
+
+        try:
+            pdt.assert_frame_equal(
+                actual_csv,
+                expected_csv,
+            )
+        except AssertionError as pandas_error:
+            print(f"Mismatch in file {actual_file_path}:", file=sys.stderr)
+            print(pandas_error, file=sys.stderr)
+            snapshot.assert_match_dir(
+                generate_snapshot_dictionary(output_path), "known_good_output"
+            )
+
+
+def test_sth_end_to_end_no_historic_many_worms(snapshot):
+    data_dir = "data_no_historic"
+    test_root = Path(__file__).parent / data_dir
+    input_data = test_root / "example_input_data"
+    output_path = test_root / "generated_data_combined_worms"
+
+    if output_path.exists():
+        shutil.rmtree(output_path)
+
+    run_sch.run_sth_postprocessing_pipeline(
+        f"{input_data}/",  # currently requires trailing slash
+        output_dir=output_path,
+        worm_directories=[
+            "hookworm",
+            "ascaris",
+        ],
+        num_jobs=1,
+        run_country_level_summaries=True,
+    )
+
+    # # Composite data is not part of the interface so don't check
+    composite_path = output_path / "composite"
+    shutil.rmtree(composite_path)
+
+    results = sorted(generate_flat_snapshot_set(output_path))
+    expected_results = sorted(
+        generate_flat_snapshot_set(test_root / "known_good_output_combined_worms")
+    )
+
+    snapshot.snapshot_dir = test_root
+
+    if results != expected_results:
+        snapshot.assert_match_dir(
+            generate_snapshot_dictionary(output_path),
+            "known_good_output_combined_worms",
+        )
+        # If updating we want to stop here
+        return
+
+    for actual_file_path, expected_file_path in zip(results, expected_results):
+        assert actual_file_path == expected_file_path
+        full_actual_path = output_path / actual_file_path
+        full_expected_file_path = (
+            test_root / "known_good_output_combined_worms" / expected_file_path
+        )
+
+        actual_csv = pd.read_csv(full_actual_path)
+        expected_csv = pd.read_csv(full_expected_file_path)
+
+        try:
+            pdt.assert_frame_equal(
+                actual_csv,
+                expected_csv,
+            )
+        except AssertionError as pandas_error:
+            print(f"Mismatch in file {actual_file_path}:", file=sys.stderr)
+            print(pandas_error, file=sys.stderr)
+            snapshot.assert_match_dir(
+                generate_snapshot_dictionary(output_path),
+                "known_good_output_combined_worms",
+            )
