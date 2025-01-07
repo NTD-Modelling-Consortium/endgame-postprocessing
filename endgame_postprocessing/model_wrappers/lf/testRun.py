7a453fcfde11c055d2a19804ceea10e9c478edcf
Thomas <thomas.kiley@ndm.ox.ac.uk>
2025-01-07 14:52:51 +0000

    Adjust the historic combination to take from specified scenario
    Rather than having a seperate source of data we are now using
    a specified scenario that ran from the origin as the basis

--- endgame_postprocessing/model_wrappers/lf/testRun.py
+++ endgame_postprocessing/model_wrappers/lf/testRun.py
@@ -1,10 +1,6 @@
 from collections import defaultdict
 from tqdm import tqdm
-from endgame_postprocessing.post_processing import combine_historic_and_forward
 from endgame_postprocessing.post_processing.canonical_results import CanonicalResults
-from endgame_postprocessing.model_wrappers.lf.historic_standardise_step import (
-    perform_historic_standardise_step,
-)
 from endgame_postprocessing.post_processing import (
     canonicalise,
     output_directory_structure,
@@ -15,6 +11,7 @@ from endgame_postprocessing.post_processing.disease import Disease
 import pandas as pd
 
 from endgame_postprocessing.post_processing.pipeline_config import PipelineConfig
+from endgame_postprocessing.post_processing.replicate_historic_data_from_scenario import replicate_historic_data_in_all_scenarios
 
 def get_lf_standard(input_dir):
     return file_util.get_flat_regex(
@@ -54,21 +51,18 @@ def write_canonical_results(results: CanonicalResults, output_dir):
 
 def run_postprocessing_pipeline(
     forward_projection_raw: str,
-    historic_data_nonstandard: str,
+    scenario_with_historic_data: str,
     output_dir: str,
     num_jobs: int,
 ):
     """
     Aggregates into standard format the input files found in forward_projection_raw.
-    Appended to with historic data found in historic_data_nonstandard
+    Data specified in scenario_with_historic_data will be prepended to all other scenarios 
+    (use None to disable this behaviour)
     forward_projection_raw should contain files (in any structure with names matching):
         - `ntdmc-AAA12345-lf-scenario_0-200.csv`
             for IU AAA12345 and scenario 0
         - `PopulationMetadatafile.csv` (must be at the root)
-
-    historic_data_nonstandard should contain the files Matt generated for historic data
-    which contain just a year column and then draw_0,... where the values are the
-    protocol prevalence.
 
     The output directory must be empty.
     On completion the sub-structure will be:
@@ -83,27 +77,14 @@ def run_postprocessing_pipeline(
             combined-lf-africa-lvl-agg.csv - aggregated across Africa
     Arguments:
         forward_projection_raw (str): The directory to search for input files.
-        historic_data_nonstandard (str): The directory to search for historic input files
+        scenario_with_historic_data (str): The name of the scenario to use for historic data
         output_dir (str): The directory to store the output files.
 
     """
-    if historic_data_nonstandard is not None:
-        historic_raw_path = f"{output_dir}/historic_only/raw"
-        perform_historic_standardise_step(historic_data_nonstandard, historic_raw_path)
-
-        forward_canonical = f"{output_dir}/forward_only/canonical"
-        forward_results = canonicalise_raw_lf_results(forward_projection_raw)
-        write_canonical_results(forward_results, forward_canonical)
-        historic_canonical = f"{output_dir}/historic_only/canonical"
-        historic_results = canonicalise_raw_lf_results(historic_raw_path)
-        write_canonical_results(historic_results, historic_canonical)
-
-        combine_historic_and_forward.combine_historic_and_forward(
-            historic_canonical, forward_canonical, output_dir
-        )
-    else:
-        results = canonicalise_raw_lf_results(forward_projection_raw)
-        write_canonical_results(results, output_dir)
+    results = canonicalise_raw_lf_results(forward_projection_raw)
+    if scenario_with_historic_data is not None:
+        results = replicate_historic_data_in_all_scenarios(results, scenario_with_historic_data)
+    write_canonical_results(results, output_dir)
 
 
     pipeline.pipeline(
