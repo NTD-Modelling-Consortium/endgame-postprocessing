2685346a76071a500805e263202ff28c8c0f838d
Thomas <thomas.kiley@ndm.ox.ac.uk>
2025-01-07 17:22:04 +0000

    Exclude IUs that are missing in any scenario

--- endgame_postprocessing/post_processing/replicate_historic_data_from_scenario.py
+++ endgame_postprocessing/post_processing/replicate_historic_data_from_scenario.py
@@ -8,13 +8,16 @@ def replicate_historic_data_in_all_scenarios(results: CanonicalResults, source_s
     if source_scenario not in results:
         raise ValueError(f"Invalid source_scenario: '{source_scenario}' as not in {list(results.keys())}") # noqa E501
     updated_results = defaultdict(dict)
+    ius_to_exclude = []
     for other_scenario in results:
         if other_scenario == source_scenario:
             updated_results[other_scenario] = results[other_scenario]
             continue
+        updated_results[other_scenario] = {}
         for iu in results[source_scenario]:
             if iu not in results[other_scenario]:
                 warnings.warn(f"IU {iu} found in {source_scenario} but not found in {other_scenario}") # noqa E501
+                ius_to_exclude.append(iu)
                 continue
             _, source_scenario_data = results[source_scenario][iu]
             other_scenario_file, other_scenario_data = results[other_scenario][iu]
@@ -32,6 +35,11 @@ def replicate_historic_data_in_all_scenarios(results: CanonicalResults, source_s
         ius_without_historic_data = results[scenario].keys() - results[source_scenario].keys()
         for iu in ius_without_historic_data:
             warnings.warn(f"IU {iu} was not found in {source_scenario} and as such will not have the historic data") # noqa E501
-            updated_results[scenario][iu] = results[scenario][iu]
+            ius_to_exclude.append(iu)
+
+    for excluded_iu in ius_to_exclude:
+        for scenario in updated_results:
+            if excluded_iu in updated_results[scenario]:
+                del updated_results[scenario][excluded_iu]
 
     return updated_results
--- tests/post_processing/test_replicate_historic_data_from_scenario.py
+++ tests/post_processing/test_replicate_historic_data_from_scenario.py
@@ -91,8 +91,7 @@ def test_replicate_historic_data_missing_forward_iu():
             "IU AAA00001 found in scenario_-1 but not found in scenario_1"
         ]
 
-    assert modified_results["scenario_-1"]["AAA00001"] == (source_file_info, source_scenario)
-    assert "scenario_1" not in modified_results
+    assert modified_results == {"scenario_-1": {}, "scenario_1":{}}
 
 def test_replicate_historic_data_missing_iu_from_history():
     target_scenario = pd.DataFrame({
@@ -115,11 +114,102 @@ def test_replicate_historic_data_missing_iu_from_history():
             "IU AAA00001 was not found in scenario_-1 and as such will not have the historic data"
         ]
 
-    assert modified_results == {
-        "scenario_-1": {},
+    assert modified_results == {"scenario_-1": {}, "scenario_1":{}}
+
+
+def test_replicate_historic_data_missing_iu_from_one_scenario():
+    source_scenario = pd.DataFrame({
+        canoncical_columns.YEAR_ID: [2010, 2011, 2012],
+        canoncical_columns.SCENARIO: ["scenario_-1"] * 3,
+        "draw_0": [0.1, 0.2, 0.3]
+    })
+    source_file_info = CustomFileInfo(scenario="scenario_-1", scenario_index=0, total_scenarios=1,
+                                      country="AAA", iu="AAA00001", file_path='')
+    target_scenario = pd.DataFrame({
+        canoncical_columns.YEAR_ID: [2012],
+        canoncical_columns.SCENARIO: ["scenario_1"],
+        "draw_0": [0.4]
+    })
+    target_file_info = CustomFileInfo(scenario="scenario_1", scenario_index=0, total_scenarios=1,
+                                      country="AAA", iu="AAA00001", file_path='')
+
+    results = {
+        "scenario_-1": {"AAA00001": (source_file_info, source_scenario)},
         "scenario_1": {"AAA00001": (target_file_info,target_scenario)},
+        "scenario_2": {},
     }
 
+    modified_results = replicate_historic_data_in_all_scenarios(results, 'scenario_-1')
+
+    assert modified_results == {"scenario_-1": {}, "scenario_1":{}, "scenario_2":{}}
+
+
+def test_replicate_historic_data_missing_iu_from_one_scenario_different_iu_present():
+    source_scenario1 = pd.DataFrame({
+        canoncical_columns.YEAR_ID: [2010, 2011, 2012],
+        canoncical_columns.SCENARIO: ["scenario_-1"] * 3,
+        "draw_0": [0.1, 0.2, 0.3]
+    })
+    source_file_info1 = CustomFileInfo(scenario="scenario_-1", scenario_index=0, total_scenarios=1,
+                                      country="AAA", iu="AAA00001", file_path='')
+
+    source_scenario2 = pd.DataFrame({
+        canoncical_columns.YEAR_ID: [2010, 2011, 2012],
+        canoncical_columns.SCENARIO: ["scenario_-1"] * 3,
+        "draw_0": [0.1, 0.2, 0.3]
+    })
+    source_file_info2 = CustomFileInfo(scenario="scenario_-1", scenario_index=0, total_scenarios=1,
+                                      country="AAA", iu="AAA00002", file_path='')
+
+    scenario1_data1 = pd.DataFrame({
+        canoncical_columns.YEAR_ID: [2012],
+        canoncical_columns.SCENARIO: ["scenario_1"],
+        "draw_0": [0.4]
+    })
+    scenario1_data2 = pd.DataFrame({
+        canoncical_columns.YEAR_ID: [2012],
+        canoncical_columns.SCENARIO: ["scenario_1"],
+        "draw_0": [0.4]
+    })
+    scenario2_data2 = pd.DataFrame({
+        canoncical_columns.YEAR_ID: [2012],
+        canoncical_columns.SCENARIO: ["scenario_2"],
+        "draw_0": [0.5]
+    })
+    scenario1_file_info1 = CustomFileInfo(scenario="scenario_1", scenario_index=0, total_scenarios=1,
+                                      country="AAA", iu="AAA00001", file_path='')
+    scenario1_file_info2 = CustomFileInfo(scenario="scenario_1", scenario_index=0, total_scenarios=1,
+                                      country="AAA", iu="AAA00002", file_path='')
+    scenario2_file_info2 = CustomFileInfo(scenario="scenario_2", scenario_index=0, total_scenarios=1,
+                                      country="AAA", iu="AAA00002", file_path='')
+
+
+    results = {
+        "scenario_-1": {"AAA00001": (source_file_info1, source_scenario1),
+                        "AAA00002": (source_file_info2, source_scenario2)},
+        "scenario_1": {"AAA00001": (scenario1_file_info1, scenario1_data1),
+                       "AAA00002": (scenario1_file_info2, scenario1_data2)},
+        "scenario_2": {"AAA00002": (scenario2_file_info2, scenario2_data2)},
+    }
+
+    modified_results = replicate_historic_data_in_all_scenarios(results, 'scenario_-1')
+
+    assert modified_results["scenario_-1"]["AAA00002"] == (source_file_info2, source_scenario2)
+    assert "AAA00001" not in modified_results["scenario_-1"]
+
+    scenario1_modified_file_info, scenario1_modified_data  = modified_results["scenario_1"]["AAA00002"]
+    assert scenario1_modified_file_info == scenario1_file_info2
+    pdt.assert_frame_equal(scenario1_modified_data.reset_index(drop=True), pd.DataFrame({
+            canoncical_columns.YEAR_ID: [2010, 2011, 2012],
+            canoncical_columns.SCENARIO: ["scenario_1"] * 3,
+            "draw_0": [0.1, 0.2, 0.4]}))
+
+    scenario_2_modified_target_file_info, scenario_2_modified_target_data  = modified_results["scenario_2"]["AAA00002"]
+    assert scenario_2_modified_target_file_info == scenario2_file_info2
+    pdt.assert_frame_equal(scenario_2_modified_target_data.reset_index(drop=True), pd.DataFrame({
+            canoncical_columns.YEAR_ID: [2010, 2011, 2012],
+            canoncical_columns.SCENARIO: ["scenario_2"] * 3,
+            "draw_0": [0.1, 0.2, 0.5]}))
 
 def test_replicate_historic_data_in_all_scenarios_scenario_missing_raises_exception():
     with pytest.raises(Exception) as e:
