74c6ac788acf099cd24dd4c39682b82694cec1ad
Thomas <thomas.kiley@ndm.ox.ac.uk>
2025-01-09 11:48:15 +0000

    Introduce helper functions to make tests easier to read
    Reduces boilerplate duplicated in tests so easier to
    see what values change with each test

--- tests/post_processing/test_replicate_historic_data_from_scenario.py
+++ tests/post_processing/test_replicate_historic_data_from_scenario.py
@@ -6,82 +6,72 @@ from endgame_postprocessing.post_processing.custom_file_info import CustomFileIn
 from endgame_postprocessing.post_processing.replicate_historic_data_from_scenario import replicate_historic_data_in_all_scenarios # noqa E501
 from endgame_postprocessing.post_processing import canoncical_columns
 
-def test_replicate_historic_data_in_all_scenarios_one_scenario_copied_to_other():
+def _generate_canonical_result(scenario, years, prevalence, iu):
     source_scenario = pd.DataFrame({
-        canoncical_columns.YEAR_ID: [2010, 2011, 2012],
-        canoncical_columns.SCENARIO: ["scenario_-1"] * 3,
-        "draw_0": [0.1, 0.2, 0.3]
+        canoncical_columns.YEAR_ID: years,
+        canoncical_columns.SCENARIO: [scenario] * len(years),
+        "draw_0": prevalence
     })
-    source_file_info = CustomFileInfo(scenario="scenario_-1", scenario_index=0, total_scenarios=1,
-                                      country="AAA", iu="AAA00001", file_path='')
-    target_scenario = pd.DataFrame({
-        canoncical_columns.YEAR_ID: [2012],
-        canoncical_columns.SCENARIO: ["scenario_1"],
-        "draw_0": [0.4]
-    })
-    target_file_info = CustomFileInfo(scenario="scenario_1", scenario_index=0, total_scenarios=1,
-                                      country="AAA", iu="AAA00001", file_path='')
+    file_info = CustomFileInfo(scenario=scenario, scenario_index=0, total_scenarios=1,
+                                      country=iu[:3], iu=iu, file_path='')
+    return (file_info, source_scenario)
+
+def _assert_results_match(results, expected_results):
+    assert results.keys() == expected_results.keys()
+    for scenario in results:
+        assert results[scenario].keys() == expected_results[scenario].keys()
+        for iu in results[scenario]:
+            result_file_info, result_data = results[scenario][iu]
+            expected_file_info, expected_data = expected_results[scenario][iu]
+            assert result_file_info == expected_file_info
+            pdt.assert_frame_equal(result_data.reset_index(drop=True), expected_data)
+
+def test_replicate_historic_data_in_all_scenarios_one_scenario_copied_to_other():
+    source_result  = _generate_canonical_result(
+        "scenario_-1", years = [2010, 2011, 2012], prevalence=[0.1, 0.2, 0.3], iu="AAA00001")
+    (target_file_info, target_data) = _generate_canonical_result(
+        "scenario_1", years=[2012], prevalence=[0.4], iu="AAA00001")
 
     results = {
-        "scenario_-1": {"AAA00001": (source_file_info, source_scenario)},
-        "scenario_1": {"AAA00001": (target_file_info,target_scenario)},
+        "scenario_-1": {"AAA00001": source_result},
+        "scenario_1": {"AAA00001": (target_file_info, target_data)},
     }
 
     modified_results = replicate_historic_data_in_all_scenarios(results, 'scenario_-1')
 
-    assert modified_results["scenario_-1"]["AAA00001"] == (source_file_info, source_scenario)
-    modified_target_file_info, modified_target_data  = modified_results["scenario_1"]["AAA00001"]
-    assert modified_target_file_info == target_file_info
-    pdt.assert_frame_equal(modified_target_data.reset_index(drop=True), pd.DataFrame({
+    _assert_results_match(modified_results, {
+        "scenario_-1": { "AAA00001": source_result},
+        "scenario_1": { "AAA00001": (target_file_info, pd.DataFrame({
             canoncical_columns.YEAR_ID: [2010, 2011, 2012],
             canoncical_columns.SCENARIO: ["scenario_1"] * 3,
             "draw_0": [0.1, 0.2, 0.4]
-    }))
+    }))}})
 
 def test_replicate_historic_data_with_non_overlapping_data():
-    source_scenario = pd.DataFrame({
-        canoncical_columns.YEAR_ID: [2010, 2011, 2012],
-        canoncical_columns.SCENARIO: ["scenario_-1"] * 3,
-        "draw_0": [0.1, 0.2, 0.3]
-    })
-    source_file_info = CustomFileInfo(scenario="scenario_-1", scenario_index=0, total_scenarios=1,
-                                      country="AAA", iu="AAA00001", file_path='')
-    target_scenario = pd.DataFrame({
-        canoncical_columns.YEAR_ID: [2014],
-        canoncical_columns.SCENARIO: ["scenario_1"],
-        "draw_0": [0.4]
-    })
-    target_file_info = CustomFileInfo(scenario="scenario_1", scenario_index=0, total_scenarios=1,
-                                      country="AAA", iu="AAA00001", file_path='')
+    source_result  = _generate_canonical_result(
+        "scenario_-1", years = [2010, 2011, 2012], prevalence=[0.1, 0.2, 0.3], iu="AAA00001")
+    (target_file_info, target_data) = _generate_canonical_result(
+        "scenario_1", years=[2014], prevalence=[0.4], iu="AAA00001")
 
     results = {
-        "scenario_-1": {"AAA00001": (source_file_info, source_scenario)},
-        "scenario_1": {"AAA00001": (target_file_info,target_scenario)},
+        "scenario_-1": {"AAA00001": source_result},
+        "scenario_1": {"AAA00001": (target_file_info,target_data)},
     }
 
     modified_results = replicate_historic_data_in_all_scenarios(results, 'scenario_-1')
-
-    assert modified_results["scenario_-1"]["AAA00001"] == (source_file_info, source_scenario)
-    modified_target_file_info, modified_target_data  = modified_results["scenario_1"]["AAA00001"]
-    assert modified_target_file_info == target_file_info
-    pdt.assert_frame_equal(modified_target_data.reset_index(drop=True), pd.DataFrame({
+    _assert_results_match(modified_results, {
+        "scenario_-1": { "AAA00001": source_result},
+        "scenario_1": { "AAA00001": (target_file_info, pd.DataFrame({
             canoncical_columns.YEAR_ID: [2010, 2011, 2012, 2014],
             canoncical_columns.SCENARIO: ["scenario_1"] * 4,
             "draw_0": [0.1, 0.2, 0.3, 0.4]
-    }))
+    }))}})
 
 def test_replicate_historic_data_missing_forward_iu():
-    source_scenario = pd.DataFrame({
-        canoncical_columns.YEAR_ID: [2010, 2011, 2012],
-        canoncical_columns.SCENARIO: ["scenario_-1"] * 3,
-        "draw_0": [0.1, 0.2, 0.3]
-    })
-    source_file_info = CustomFileInfo(
-        scenario="scenario_-1", scenario_index=0, total_scenarios=1,
-        country="AAA", iu="AAA00001", file_path='')
-
+    source_result  = _generate_canonical_result(
+        "scenario_-1", years = [2010, 2011, 2012], prevalence=[0.1, 0.2, 0.3], iu="AAA00001")
     results = {
-        "scenario_-1": {"AAA00001": (source_file_info, source_scenario)},
+        "scenario_-1": {"AAA00001": source_result},
         "scenario_1": {},
     }
 
@@ -94,18 +84,12 @@ def test_replicate_historic_data_missing_forward_iu():
     assert modified_results == {"scenario_-1": {}, "scenario_1":{}}
 
 def test_replicate_historic_data_missing_iu_from_history():
-    target_scenario = pd.DataFrame({
-        canoncical_columns.YEAR_ID: [2012],
-        canoncical_columns.SCENARIO: ["scenario_1"],
-        "draw_0": [0.4]
-    })
-    target_file_info = CustomFileInfo(
-        scenario="scenario_1", scenario_index=0, total_scenarios=1,
-        country="AAA", iu="AAA00001", file_path='')
+    (target_file_info, target_data) = _generate_canonical_result(
+        "scenario_1", years=[2012], prevalence=[0.4], iu="AAA00001")
 
     results = {
         "scenario_-1": {},
-        "scenario_1": {"AAA00001": (target_file_info,target_scenario)},
+        "scenario_1": {"AAA00001": (target_file_info,target_data)},
     }
 
     with warnings.catch_warnings(record=True) as w:
@@ -118,24 +102,14 @@ def test_replicate_historic_data_missing_iu_from_history():
 
 
 def test_replicate_historic_data_missing_iu_from_one_scenario():
-    source_scenario = pd.DataFrame({
-        canoncical_columns.YEAR_ID: [2010, 2011, 2012],
-        canoncical_columns.SCENARIO: ["scenario_-1"] * 3,
-        "draw_0": [0.1, 0.2, 0.3]
-    })
-    source_file_info = CustomFileInfo(scenario="scenario_-1", scenario_index=0, total_scenarios=1,
-                                      country="AAA", iu="AAA00001", file_path='')
-    target_scenario = pd.DataFrame({
-        canoncical_columns.YEAR_ID: [2012],
-        canoncical_columns.SCENARIO: ["scenario_1"],
-        "draw_0": [0.4]
-    })
-    target_file_info = CustomFileInfo(scenario="scenario_1", scenario_index=0, total_scenarios=1,
-                                      country="AAA", iu="AAA00001", file_path='')
+    source_result = _generate_canonical_result(
+        "scenario_-1", years=[2010, 2011, 2012], prevalence=[0.1, 0.2, 0.3], iu="AAA00001")
+    target_result = _generate_canonical_result(
+        "scenario_1", years=[2012], prevalence=[0.4], iu="AAA00001")
 
     results = {
-        "scenario_-1": {"AAA00001": (source_file_info, source_scenario)},
-        "scenario_1": {"AAA00001": (target_file_info,target_scenario)},
+        "scenario_-1": {"AAA00001": source_result},
+        "scenario_1": {"AAA00001": target_result},
         "scenario_2": {},
     }
 
@@ -145,71 +119,40 @@ def test_replicate_historic_data_missing_iu_from_one_scenario():
 
 
 def test_replicate_historic_data_missing_iu_from_one_scenario_different_iu_present():
-    source_scenario1 = pd.DataFrame({
-        canoncical_columns.YEAR_ID: [2010, 2011, 2012],
-        canoncical_columns.SCENARIO: ["scenario_-1"] * 3,
-        "draw_0": [0.1, 0.2, 0.3]
-    })
-    source_file_info1 = CustomFileInfo(scenario="scenario_-1", scenario_index=0, total_scenarios=1,
-                                      country="AAA", iu="AAA00001", file_path='')
+    source_result1 = _generate_canonical_result(
+        "scenario_-1", years=[2010, 2011, 2012], prevalence=[0.1, 0.2, 0.3], iu="AAA00001")
 
-    source_scenario2 = pd.DataFrame({
-        canoncical_columns.YEAR_ID: [2010, 2011, 2012],
-        canoncical_columns.SCENARIO: ["scenario_-1"] * 3,
-        "draw_0": [0.1, 0.2, 0.3]
-    })
-    source_file_info2 = CustomFileInfo(scenario="scenario_-1", scenario_index=0, total_scenarios=1,
-                                      country="AAA", iu="AAA00002", file_path='')
+    source_result2 = _generate_canonical_result(
+        "scenario_-1", years=[2010, 2011, 2012], prevalence=[0.1, 0.2, 0.3], iu="AAA00002")
 
-    scenario1_data1 = pd.DataFrame({
-        canoncical_columns.YEAR_ID: [2012],
-        canoncical_columns.SCENARIO: ["scenario_1"],
-        "draw_0": [0.4]
-    })
-    scenario1_data2 = pd.DataFrame({
-        canoncical_columns.YEAR_ID: [2012],
-        canoncical_columns.SCENARIO: ["scenario_1"],
-        "draw_0": [0.4]
-    })
-    scenario2_data2 = pd.DataFrame({
-        canoncical_columns.YEAR_ID: [2012],
-        canoncical_columns.SCENARIO: ["scenario_2"],
-        "draw_0": [0.5]
-    })
-    scenario1_file_info1 = CustomFileInfo(scenario="scenario_1", scenario_index=0, total_scenarios=1,
-                                      country="AAA", iu="AAA00001", file_path='')
-    scenario1_file_info2 = CustomFileInfo(scenario="scenario_1", scenario_index=0, total_scenarios=1,
-                                      country="AAA", iu="AAA00002", file_path='')
-    scenario2_file_info2 = CustomFileInfo(scenario="scenario_2", scenario_index=0, total_scenarios=1,
-                                      country="AAA", iu="AAA00002", file_path='')
-
+    scenario1_result1 = _generate_canonical_result(
+        "scenario_1", years=[2012], prevalence=[0.4], iu="AAA00001")
+    scenario1_result2 = _generate_canonical_result(
+        "scenario_1", years=[2012], prevalence=[0.4], iu="AAA00002")
+    scenario2_result2 = _generate_canonical_result(
+        "scenario_2", years=[2012], prevalence=[0.5], iu="AAA00002")
 
     results = {
-        "scenario_-1": {"AAA00001": (source_file_info1, source_scenario1),
-                        "AAA00002": (source_file_info2, source_scenario2)},
-        "scenario_1": {"AAA00001": (scenario1_file_info1, scenario1_data1),
-                       "AAA00002": (scenario1_file_info2, scenario1_data2)},
-        "scenario_2": {"AAA00002": (scenario2_file_info2, scenario2_data2)},
+        "scenario_-1": {"AAA00001": source_result1,
+                        "AAA00002": source_result2},
+        "scenario_1": {"AAA00001": scenario1_result1,
+                       "AAA00002": scenario1_result2},
+        "scenario_2": {"AAA00002": scenario2_result2},
     }
 
     modified_results = replicate_historic_data_in_all_scenarios(results, 'scenario_-1')
 
-    assert modified_results["scenario_-1"]["AAA00002"] == (source_file_info2, source_scenario2)
-    assert "AAA00001" not in modified_results["scenario_-1"]
-
-    scenario1_modified_file_info, scenario1_modified_data  = modified_results["scenario_1"]["AAA00002"]
-    assert scenario1_modified_file_info == scenario1_file_info2
-    pdt.assert_frame_equal(scenario1_modified_data.reset_index(drop=True), pd.DataFrame({
+    _assert_results_match(modified_results, {
+        "scenario_-1": {"AAA00002": source_result2},
+        "scenario_1": {"AAA00002": (scenario1_result2[0], pd.DataFrame({
             canoncical_columns.YEAR_ID: [2010, 2011, 2012],
             canoncical_columns.SCENARIO: ["scenario_1"] * 3,
-            "draw_0": [0.1, 0.2, 0.4]}))
-
-    scenario_2_modified_target_file_info, scenario_2_modified_target_data  = modified_results["scenario_2"]["AAA00002"]
-    assert scenario_2_modified_target_file_info == scenario2_file_info2
-    pdt.assert_frame_equal(scenario_2_modified_target_data.reset_index(drop=True), pd.DataFrame({
+            "draw_0": [0.1, 0.2, 0.4]}))},
+        "scenario_2": {"AAA00002": (scenario2_result2[0], pd.DataFrame({
             canoncical_columns.YEAR_ID: [2010, 2011, 2012],
             canoncical_columns.SCENARIO: ["scenario_2"] * 3,
-            "draw_0": [0.1, 0.2, 0.5]}))
+            "draw_0": [0.1, 0.2, 0.5]}))},
+    })
 
 def test_replicate_historic_data_in_all_scenarios_scenario_missing_raises_exception():
     with pytest.raises(Exception) as e:
