f8efd95fb356cc94e1f8df0fa4c7bba24b12bef2
Thomas <thomas.kiley@ndm.ox.ac.uk>
2025-01-20 11:08:04 +0000

    Rename function for renaming historic files
    Only supports STH, SCH and other diseases will need to be different

--- endgame_postprocessing/model_wrappers/sch/rename_historic.py
+++ endgame_postprocessing/model_wrappers/sch/rename_historic.py
@@ -17,7 +17,8 @@ def _standard_worm_from_historic(historic_worm_name):
         raise Exception(f"Unexpected worm: {historic_worm_name}")
     return historic_worm_remapping[historic_worm_name]
 
-def get_standard_name_for_historic_file(historic_file_name):
+
+def get_standard_name_for_historic_sth_file(historic_file_name):
     old_file_name_regex = r"PrevDataset_(?P<worm>\w+)_(?P<iu_id>(?P<country>[A-Z]{3})\d{5})(?P<scenario>).csv"
     file_match = re.search(old_file_name_regex, historic_file_name)
     if not file_match:
@@ -29,14 +30,15 @@ def get_standard_name_for_historic_file(historic_file_name):
     new_file_name = _raw_file_name(iu, standard_worm_name, scenario)
     return new_file_name
 
-def rename_historic_files(historic_input_dir, historic_renamed_raw_dir):
+
+def rename_historic_sth_files(historic_input_dir, historic_renamed_raw_dir):
     Path(historic_renamed_raw_dir).mkdir(parents=True)
     files = glob.glob(
         "**/*.csv", root_dir=historic_input_dir, recursive=True
     )
 
     for file_name in files:
-        new_file_name = get_standard_name_for_historic_file(file_name)
+        new_file_name = get_standard_name_for_historic_sth_file(file_name)
         if new_file_name is None:
             continue
         old_file_path = f"{historic_input_dir}/{file_name}"
--- tests/model_wrappers/sch/test_rename_historic.py
+++ tests/model_wrappers/sch/test_rename_historic.py
@@ -22,14 +22,14 @@ import endgame_postprocessing.model_wrappers.sch.rename_historic as rename_histo
     ],
 )
 def test_get_standard_name_for_historic_file(historic_name, new_name):
-    assert new_name == rename_historic.get_standard_name_for_historic_file(
+    assert new_name == rename_historic.get_standard_name_for_historic_sth_file(
         historic_name
     )
 
 
 def test_rename_flat_historic_data(fs):
     fs.create_file("foo/PrevDataset_Hook_AAA00001.csv"),
-    rename_historic.rename_historic_files("foo", "bar")
+    rename_historic.rename_historic_sth_files("foo", "bar")
     assert os.path.exists(
         "bar/ntdmc-AAA00001-hookworm-group_001-scenario_0_survey_type_kk2-group_001-200_simulations.csv"
     )
