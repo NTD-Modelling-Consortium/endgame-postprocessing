0e4e395d3c72297e462342c16e57f396f4381314
Chatrapathi Akula <chatrapathi.akula@gmail.com>
2025-01-20 15:40:20 +0000

    Prepend historic to forward projection trachoma data and handle historic data with missing IUs

--- endgame_postprocessing/model_wrappers/trachoma/run_trach.py
+++ endgame_postprocessing/model_wrappers/trachoma/run_trach.py
@@ -1,5 +1,6 @@
 from os import PathLike
 from pathlib import Path
+from typing import Optional
 
 import pandas as pd
 from tqdm import tqdm
@@ -9,14 +10,18 @@ from endgame_postprocessing.post_processing import (
     output_directory_structure,
     pipeline,
     file_util,
+    canoncical_columns,
 )
 from endgame_postprocessing.post_processing.disease import Disease
+from endgame_postprocessing.post_processing.file_util import get_matching_csv
 from endgame_postprocessing.post_processing.pipeline_config import PipelineConfig
 
 
 def canonicalise_raw_trachoma_results(
     input_dir: str | PathLike | Path,
     output_dir: str | PathLike | Path,
+    historic_dir: Optional[str | PathLike | Path] = None,
+    historic_prefix: str = "",
     start_year: int = 1970,
     stop_year: int = 2041,
 ):
@@ -37,9 +42,23 @@ def canonicalise_raw_trachoma_results(
         raw_iu = pd.read_csv(file_info.file_path)
 
         # TODO(16.1.2025): Implement historic data handling here
+        if historic_dir is not None:
+            historic_iu_file_path = get_matching_csv(
+                historic_dir,
+                historic_prefix,
+                file_info.country,
+                file_info.iu.replace(file_info.country, ""),
+            )
+            raw_iu_historic = pd.read_csv(historic_iu_file_path)
+            raw_iu = pd.concat([raw_iu_historic, raw_iu])
+
         raw_iu_filtered = raw_iu[
-            (raw_iu["year_id"] >= start_year) & (raw_iu["year_id"] <= stop_year)
+            (raw_iu["Time"] >= start_year) & (raw_iu["Time"] <= stop_year)
         ].copy()
+
+        raw_iu_filtered = raw_iu_filtered.rename(
+            columns={"Time": canoncical_columns.YEAR_ID}
+        )
 
         # TODO: canonical shouldn't need the age_start / age_end but these are assumed present later
         canonical_result = canonicalise.canonicalise_raw(
@@ -55,17 +74,21 @@ def canonicalise_raw_trachoma_results(
 def run_postprocessing_pipeline(
     input_dir: str | PathLike | Path,
     output_dir: str | PathLike | Path,
+    historic_dir: Optional[str | PathLike | Path] = None,
+    historic_prefix: str = "",
     start_year: int = 1970,
     stop_year: int = 2041,
 ):
     canonicalise_raw_trachoma_results(
         input_dir=input_dir,
         output_dir=output_dir,
+        historic_dir=historic_dir,
+        historic_prefix=historic_prefix,
         start_year=start_year,
         stop_year=stop_year,
     )
     pipeline.pipeline(
         input_dir=input_dir,
         working_directory=output_dir,
-        pipeline_config=PipelineConfig(disease=Disease.TRACHOMA),
+        pipeline_config=PipelineConfig(disease=Disease.TRACHOMA, threshold=0.05),
     )
