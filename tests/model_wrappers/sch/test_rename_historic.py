d592dbe0908815016c9e183f1ee1cb38f4409f88
Thomas <thomas.kiley@ndm.ox.ac.uk>
2025-01-20 11:11:08 +0000

    Fix test file name
    STH does not include the survey type

--- tests/model_wrappers/sch/test_rename_historic.py
+++ tests/model_wrappers/sch/test_rename_historic.py
@@ -31,6 +31,6 @@ def test_rename_flat_historic_data(fs):
     fs.create_file("foo/PrevDataset_Hook_AAA00001.csv"),
     rename_historic.rename_historic_sth_files("foo", "bar")
     assert os.path.exists(
-        "bar/ntdmc-AAA00001-hookworm-group_001-scenario_0_survey_type_kk2-group_001-200_simulations.csv"
+        "bar/ntdmc-AAA00001-hookworm-group_001-scenario_0-group_001-200_simulations.csv"
     )
     assert os.path.exists("foo/PrevDataset_Hook_AAA00001.csv")
