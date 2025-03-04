29eb3d6da12e3893b82a3c52f4885a6f09964692
Thomas <thomas.kiley@ndm.ox.ac.uk>
2025-03-04 18:01:33 +0000

    Collect all warnings

--- endgame_postprocessing/model_wrappers/oncho/quick_run.py
+++ endgame_postprocessing/model_wrappers/oncho/quick_run.py
@@ -1,3 +1,4 @@
+import itertools
 from multiprocessing import Pool
 import os
 
@@ -10,6 +11,9 @@ from endgame_postprocessing.post_processing import (
 )
 from endgame_postprocessing.post_processing.custom_file_info import CustomFileInfo
 from endgame_postprocessing.post_processing.disease import Disease
+from endgame_postprocessing.post_processing.generation_metadata import (
+    produce_generation_metadata,
+)
 from endgame_postprocessing.post_processing.pipeline_config import PipelineConfig
 from endgame_postprocessing.post_processing.warnings_collector import (
     CollectAndPrintWarnings,
@@ -36,18 +40,22 @@ def canonicalise_one(file_info):
 
 if __name__ == "__main__":
     input_dir = "/home/thomas/dev/data/epioncho-old-data/ntdmc-data-wrangler/output"
-    # all_files = sorted(list(file_util.get_flat_regex(
-    #     r"ntdmc-(?P<iu_id>(?P<country>[A-Z]{3})\d{5})-(?P<scenario>scenario_\w+)-200_sims.csv",
-    #     input_dir,
-    # )))
     all_files = sorted(
         list(
             file_util.get_flat_regex(
-                r"ntdmc-(?P<iu_id>(?P<country>TGO)48688)-(?P<scenario>old_scenario_-1)-200_sims.csv",
+                r"ntdmc-(?P<iu_id>(?P<country>[A-Z]{3})\d{5})-(?P<scenario>old_scenario_-?\w+)-200_sims.csv",
                 input_dir,
             )
         )
     )
+    # all_files = sorted(
+    #     list(
+    #         file_util.get_flat_regex(
+    #             r"ntdmc-(?P<iu_id>(?P<country>TGO)\d{5})-(?P<scenario>old_scenario_\w+)-200_sims.csv",
+    #             input_dir,
+    #         )
+    #     )
+    # )
 
     if len(all_files) == 0:
         raise Exception(
@@ -56,8 +64,12 @@ if __name__ == "__main__":
     os.makedirs("local_data/epioncho-2025-03-04", exist_ok=True)
     with Pool() as p:
         warnings_from_each_iu = p.map(canonicalise_one, all_files)
-    all_warnings = [[w for w in warning_list] for warning_list in warnings_from_each_iu]
+    all_canoncical_warnings = list(itertools.chain.from_iterable(warnings_from_each_iu))
+    # all_canoncical_warnings = list(itertools.chain.from_iterable([]))
     output_dir = "local_data/epioncho-2025-03-04"
     with CollectAndPrintWarnings() as collected_warnings:
-
         pipeline.pipeline(input_dir, output_dir, PipelineConfig(disease=Disease.ONCHO))
+    all_warnings = all_canoncical_warnings + collected_warnings
+    output_directory_structure.write_results_metadata_file(
+        output_dir, produce_generation_metadata(warnings=all_warnings)
+    )
