2f76d3ea8a7eeeaec5db0d32b4b600394e92b03e
Thomas <thomas.kiley@ndm.ox.ac.uk>
2025-03-04 16:25:44 +0000

    Adding a script to aggregate from new location

---
+++ endgame_postprocessing/model_wrappers/oncho/quick_run.py
@@ -0,0 +1,63 @@
+from multiprocessing import Pool
+import os
+
+import pandas as pd
+from endgame_postprocessing.post_processing import (
+    canonicalise,
+    file_util,
+    output_directory_structure,
+    pipeline,
+)
+from endgame_postprocessing.post_processing.custom_file_info import CustomFileInfo
+from endgame_postprocessing.post_processing.disease import Disease
+from endgame_postprocessing.post_processing.pipeline_config import PipelineConfig
+from endgame_postprocessing.post_processing.warnings_collector import (
+    CollectAndPrintWarnings,
+)
+
+
+def canonicalise_one(file_info):
+    output_dir = "local_data/epioncho-2025-03-04"
+    start_year = 1970
+    stop_year = 2041
+    with CollectAndPrintWarnings() as collected_warnings:
+        raw_iu = pd.read_csv(file_info.file_path)
+        raw_iu_filtered = raw_iu[
+            (raw_iu["year_id"] >= start_year) & (raw_iu["year_id"] <= stop_year)
+        ]
+        canonical_result = canonicalise.canonicalise_raw(
+            raw_iu_filtered, file_info, "prevalence"
+        )
+        output_directory_structure.write_canonical(
+            output_dir, file_info, canonical_result
+        )
+    return collected_warnings
+
+
+if __name__ == "__main__":
+    input_dir = "/home/thomas/dev/data/epioncho-old-data/ntdmc-data-wrangler/output"
+    # all_files = sorted(list(file_util.get_flat_regex(
+    #     r"ntdmc-(?P<iu_id>(?P<country>[A-Z]{3})\d{5})-(?P<scenario>scenario_\w+)-200_sims.csv",
+    #     input_dir,
+    # )))
+    all_files = sorted(
+        list(
+            file_util.get_flat_regex(
+                r"ntdmc-(?P<iu_id>(?P<country>TGO)48688)-(?P<scenario>old_scenario_-1)-200_sims.csv",
+                input_dir,
+            )
+        )
+    )
+
+    if len(all_files) == 0:
+        raise Exception(
+            "No data for IUs found - see above warnings and check input directory"
+        )
+    os.makedirs("local_data/epioncho-2025-03-04", exist_ok=True)
+    with Pool() as p:
+        warnings_from_each_iu = p.map(canonicalise_one, all_files)
+    all_warnings = [[w for w in warning_list] for warning_list in warnings_from_each_iu]
+    output_dir = "local_data/epioncho-2025-03-04"
+    with CollectAndPrintWarnings() as collected_warnings:
+
+        pipeline.pipeline(input_dir, output_dir, PipelineConfig(disease=Disease.ONCHO))
